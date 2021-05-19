e887cb20ee962dd5d724de3e93c91bd5499985f4
Trajanus <derektgoodwin@gmail.com>
2021-05-19 13:20:22 -0700

    Added brick script to handle damage and removal on collision.

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(Color.red == spriteRenderer.color)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+}
